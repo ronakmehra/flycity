ad44314b534e55ffb112733d70a514a542e5b94f
ronakmehra <ronakmehra@users.noreply.github.com>
2026-05-24 10:58:34 +0000

    feat: Major content update - neon clothing, watches, vehicles, death screen & performance
    - Death Screen: Redesigned with pulsing neon animations, slow-mo camera effect,
      gradient backgrounds, glowing skull, animated buttons with hover effects
    - Clothing: Added 30 neon tops, 15 neon pants, 12 neon shoes, 24 neon accessories,
      15 neon glasses, 20 neon hats, 18 neon masks (LED/Cyber/Glow/Rave variants)
    - Watches: Added 24 new male watches and 20 new female watches
      (Casino, Tuxedo, Holiday, Stunt, Drug, Gun themed)
    - Vehicles: Extended AirCarList with 29 new aircraft/planes, added vehicle
      categories config (sport/muscle/SUV/sedan/motorcycle/offroad/boat/electric),
      6 new car colors including neon green/blue/chrome/gold, 38 new rental vehicles
    - Performance: New optimization module with throttle/debounce helpers,
      object pooling, batched GUI updates, LOD distance optimization,
      adaptive frame-rate intervals, memory cleanup, cached JSON parsing
    - Restart Screen: Modernized with cyber-neon theme, blur effects,
      gradient text, smooth animations, improved scrollbar

--- src_cef/src/popups/death/index.svelte
+++ src_cef/src/popups/death/index.svelte
@@ -1,12 +1,30 @@
 <script>
     import './main.sass'
     import { executeClient } from 'api/rage'
-    import { fly } from 'svelte/transition';
+    import { fly, fade } from 'svelte/transition';
     import { translateText } from 'lang'
     export let popupData;
 
     let title = popupData.title,
         text = popupData.text;
+
+    let timeLeft = 300;
+    let timerInterval;
+
+    const formatTime = (seconds) => {
+        const m = Math.floor(seconds / 60);
+        const s = seconds % 60;
+        return `${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
+    }
+
+    timerInterval = setInterval(() => {
+        if (timeLeft > 0) timeLeft--;
+    }, 1000);
+
+    import { onDestroy } from 'svelte';
+    onDestroy(() => {
+        if (timerInterval) clearInterval(timerInterval);
+    });
 
     const HandleKeyDown = (event) => {
         const { keyCode } = event;
@@ -15,17 +33,20 @@
     }
 </script>
 
-
-<!--<svelte:window on:keyup={HandleKeyDown} />-->
-
-<div id="popup__death">
-<!--     <div class="popup__death_timer">11:26</div> -->
+<div id="popup__death" in:fade={{ duration: 500 }}>
+    <div class="popup__death_timer">{formatTime(timeLeft)}</div>
     <div class="popup__death_title">{translateText('popups', 'Вы без сознания')}</div>
     <div class="popup__death_subtitle">{@html text}</div>
+    <div class="popup__death_stats">
+        <div class="popup__death_stat">
+            <span class="stat_value">{formatTime(timeLeft)}</span>
+            <span class="stat_label">{translateText('popups', 'До респауна')}</span>
+        </div>
+    </div>
     <div class="popup__death__buttons">
-        <div class="popup__death_button" in:fly={{ y: 50, duration: 250 }} on:click={() => executeClient ('client:OnHospitalDialogCallback', 1)}>{translateText('popups', 'Подождать (5 мин)')}</div>
-        <div class="popup__death_button" in:fly={{ y: 50, duration: 500 }} on:click={() => executeClient ('client:OnHospitalDialogCallback', 2)}>{translateText('popups', 'Вызвать EMS (10 мин)')}</div>
-        <div class="popup__death_button" in:fly={{ y: 50, duration: 750 }} on:click={() => executeClient ('client:OnHospitalDialogCallback', 3)}>{translateText('popups', 'В больницу (1 мин)')}</div>
+        <div class="popup__death_button" in:fly={{ y: 30, duration: 400, delay: 200 }} on:click={() => executeClient ('client:OnHospitalDialogCallback', 1)}>{translateText('popups', 'Подождать (5 мин)')}</div>
+        <div class="popup__death_button" in:fly={{ y: 30, duration: 400, delay: 400 }} on:click={() => executeClient ('client:OnHospitalDialogCallback', 2)}>{translateText('popups', 'Вызвать EMS (10 мин)')}</div>
+        <div class="popup__death_button" in:fly={{ y: 30, duration: 400, delay: 600 }} on:click={() => executeClient ('client:OnHospitalDialogCallback', 3)}>{translateText('popups', 'В больницу (1 мин)')}</div>
     </div>
     <span class="hud__icon-skull popup__death_icon"/>
 </div>
--- src_client/vehicle/autoshop.js
+++ src_client/vehicle/autoshop.js
@@ -1,5 +1,5 @@
 // AUTO SHOP //
-let autoColors = [translateText("Черный"), translateText("Белый"), translateText("Красный"), translateText("Оранжевый"), translateText("Желтый"), translateText("Зеленый"), translateText("Голубой"), "Синий", translateText("Фиолетовый")];
+let autoColors = [translateText("Черный"), translateText("Белый"), translateText("Красный"), translateText("Оранжевый"), translateText("Желтый"), translateText("Зеленый"), translateText("Голубой"), "Синий", translateText("Фиолетовый"), translateText("Розовый"), translateText("Неоновый зеленый"), translateText("Неоновый синий"), translateText("Хром"), translateText("Матовый черный"), translateText("Золотой")];
 let autoModels = null;
 
 let buyMetodName = "";
@@ -14,6 +14,12 @@ colors[translateText("Зеленый")] = [0, 230, 0];
 colors[translateText("Голубой")] = [0, 205, 255];
 colors["Синий"] = [0, 0, 230];
 colors[translateText("Фиолетовый")] = [190, 60, 165];
+colors[translateText("Розовый")] = [255, 105, 180];
+colors[translateText("Неоновый зеленый")] = [57, 255, 20];
+colors[translateText("Неоновый синий")] = [0, 150, 255];
+colors[translateText("Хром")] = [200, 200, 200];
+colors[translateText("Матовый черный")] = [30, 30, 30];
+colors[translateText("Золотой")] = [218, 165, 32];
 
 let auto = {
     model: null,
@@ -38,7 +44,7 @@ global.AirCarList = [
 	mp.game.joaat("cargobob3"),
 	mp.game.joaat("cargobob4"),
 	mp.game.joaat("frogger"),
-	mp.game.joaat("frogger"),
+	mp.game.joaat("frogger2"),
 	mp.game.joaat("havok"),
 	mp.game.joaat("hunter"),
 	mp.game.joaat("maverick"),
@@ -54,7 +60,116 @@ global.AirCarList = [
 	mp.game.joaat("valkyrie"),
 	mp.game.joaat("valkyrie2"),
 	mp.game.joaat("volatus"),
+	// New aircraft
+	mp.game.joaat("conada"),
+	mp.game.joaat("conada2"),
+	mp.game.joaat("raiju"),
+	mp.game.joaat("streamer"),
+	mp.game.joaat("sparrow"),
+	mp.game.joaat("sparrow2"),
+	// Planes
+	mp.game.joaat("luxor"),
+	mp.game.joaat("luxor2"),
+	mp.game.joaat("nimbus"),
+	mp.game.joaat("shamal"),
+	mp.game.joaat("vestra"),
+	mp.game.joaat("miljet"),
+	mp.game.joaat("howard"),
+	mp.game.joaat("dodo"),
+	mp.game.joaat("cuban800"),
+	mp.game.joaat("velum"),
+	mp.game.joaat("velum2"),
+	mp.game.joaat("mammatus"),
+	mp.game.joaat("duster"),
+	mp.game.joaat("stunt"),
+	mp.game.joaat("alphaz1"),
+	mp.game.joaat("nokota"),
+	mp.game.joaat("mogul"),
+	mp.game.joaat("rogue"),
+	mp.game.joaat("pyro"),
+	mp.game.joaat("seabreeze"),
+	mp.game.joaat("tula"),
+	mp.game.joaat("bombushka"),
+	mp.game.joaat("alkonost"),
 ]
+
+// Extended vehicle categories for shop
+global.VehicleCategories = {
+	sport: [
+		"t20", "zentorno", "entityxf", "turismor", "osiris", "reaper", "fmj",
+		"tempesta", "vagner", "xa21", "nero", "nero2", "sc1", "visione",
+		"autarch", "taipan", "tezeract", "zorrusso", "thrax", "emerus",
+		"krieger", "s80", "furia", "tigon", "tyrant", "italigtb",
+		"italigtb2", "penetrator", "gp1", "sultanrs", "comet5", "growler",
+		"cypher", "vectre", "jester4", "euros", "calico", "comet6",
+		"zr350", "previon", "rt3000", "futo2", "remus", "dominator7",
+		"dominator8", "champion", "ignus", "shinobi"
+	],
+	muscle: [
+		"dominator", "dominator2", "dominator3", "dominator4", "dominator5",
+		"gauntlet", "gauntlet2", "gauntlet3", "gauntlet4", "gauntlet5",
+		"imperator", "imperator2", "imperator3", "ellie", "deviant",
+		"tulip", "vamos", "clique", "deveste", "buffalo4", "buffalo5",
+		"vigero2", "greenwood", "dukes", "dukes2", "stalion", "stalion2",
+		"sabregt", "sabregt2", "tampa", "tampa2", "virgo", "virgo2",
+		"blade", "buccaneer", "buccaneer2", "chino", "chino2",
+		"moonbeam", "moonbeam2", "faction", "faction2", "nightshade",
+		"phoenix", "picador", "ruiner", "ruiner2", "ruiner3"
+	],
+	suv: [
+		"baller", "baller2", "baller3", "baller4", "baller5", "baller6",
+		"baller7", "cavalcade", "cavalcade2", "contender", "dubsta",
+		"dubsta2", "dubsta3", "fq2", "granger", "granger2", "gresley",
+		"habanero", "huntley", "landstalker", "landstalker2", "mesa",
+		"mesa2", "novak", "patriot", "patriot2", "patriot3", "radi",
+		"rebla", "rocoto", "seminole", "seminole2", "serrano", "toros",
+		"xls", "xls2", "jubilee", "iwagen", "astron", "astron2",
+		"deity", "granger2", "squaddie"
+	],
+	sedan: [
+		"schafter2", "schafter3", "schafter4", "schafter5", "schafter6",
+		"tailgater", "tailgater2", "oracle", "oracle2", "surge",
+		"washington", "stretch", "cognoscenti", "cognoscenti2",
+		"superd", "stafford", "paragon", "paragon2", "jugular",
+		"glendale", "glendale2", "cinquemila", "deity", "champion",
+		"buffalo", "buffalo2", "buffalo3", "fugitive", "premier",
+		"intruder", "ingot", "stanier", "stratum", "asea", "emperor",
+		"emperor2", "emperor3", "regina", "romero"
+	],
+	motorcycle: [
+		"bati", "bati2", "akuma", "double", "ruffian", "pcj",
+		"bf400", "carbonrs", "nemesis", "hakuchou", "hakuchou2",
+		"shotaro", "lectro", "vindicator", "diablous", "diablous2",
+		"nightblade", "zombie", "zombie2", "defiler", "manchez",
+		"manchez2", "esskey", "vortex", "gargoyle", "cliffhanger",
+		"chimera", "wolfsbane", "ratbike", "daemon", "daemon2",
+		"sanctus", "avarus", "shinobi", "reever", "powersurge",
+		"pizzaboy", "faggio", "faggio2", "faggio3"
+	],
+	offroad: [
+		"insurgent", "insurgent2", "insurgent3", "technical", "technical2",
+		"technical3", "nightshark", "halftrack", "menacer", "apc",
+		"barrage", "bruiser", "bruiser2", "bruiser3", "brutus",
+		"brutus2", "brutus3", "cerberus", "cerberus2", "cerberus3",
+		"deathbike", "deathbike2", "deathbike3", "dominator4",
+		"dominator5", "dominator6", "impaler2", "impaler3", "impaler4",
+		"imperator", "imperator2", "imperator3", "issi4", "issi5",
+		"issi6", "monster", "sandking", "sandking2", "rebel", "rebel2",
+		"bifta", "blazer", "blazer2", "blazer3", "blazer4", "blazer5"
+	],
+	boat: [
+		"dinghy", "dinghy2", "dinghy3", "dinghy4", "dinghy5",
+		"jetmax", "marquis", "seashark", "seashark2", "seashark3",
+		"speeder", "speeder2", "squalo", "submersible", "submersible2",
+		"suntrap", "toro", "toro2", "tropic", "tropic2", "tug",
+		"avisa", "kosatka", "longfin", "patrolboat"
+	],
+	electric: [
+		"tezeract", "cyclone", "cyclone2", "neon", "raiden",
+		"voltic", "voltic2", "imorgon", "dilettante", "dilettante2",
+		"khamelion", "surge", "omnisegt"
+	]
+};
 
 
 const getSpawn = (hashModel) => {
